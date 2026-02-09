fbd058e5fb043a5ba02e18413024f941d1e375f1
Modularius <daniel.kirk@rocketmail.com>
2026-02-09 10:36:58 +0000

    Add BackToBackExp pulse type

--- simulator/src/integrated/simulation_elements/pulses.rs
+++ simulator/src/integrated/simulation_elements/pulses.rs
@@ -1,3 +1,5 @@
+use core::f64;
+
 use super::{FloatRandomDistribution, utils::JsonValueError};
 use digital_muon_common::{Intensity, Time};
 use serde::Deserialize;
@@ -21,11 +23,12 @@ pub(crate) enum PulseTemplate {
         peak_time: FloatRandomDistribution<f64>,
         sd: FloatRandomDistribution<f64>,
     },
-    Biexp {
-        start: FloatRandomDistribution<f64>,
+    BackToBackExp {
+        peak_height: FloatRandomDistribution<f64>,
+        peak_time: FloatRandomDistribution<f64>,
+        spread: FloatRandomDistribution<f64>,
         decay: FloatRandomDistribution<f64>,
         rise: FloatRandomDistribution<f64>,
-        height: FloatRandomDistribution<f64>,
     },
 }
 
@@ -49,14 +52,16 @@ pub(crate) enum PulseEvent {
         sd: f64,
         peak_amplitude: f64,
     },
-    Biexp {
+    BackToBackExp {
         start: f64,
         stop: f64,
-        decay: f64,
-        rise: f64,
-        peak_height: f64,
-        coef: f64,
         peak_time: f64,
+        falling: f64,
+        rising: f64,
+        normalising_factor: f64,
+        rising_spread: f64,
+        falling_spread: f64,
+        frac_1_sqrt_2_spread: f64,
     },
 }
 
@@ -97,37 +102,57 @@ impl PulseEvent {
             } => {
                 let mean = peak_time.sample(frame)?;
                 let sd = sd.sample(frame)?;
+                let peak_amplitude = height.sample(frame)?;
+                let distance_to_value_of_one = 2.0*sd*peak_amplitude.ln().sqrt();
                 Ok(Self::Gaussian {
-                    start: mean - 4.0 * sd,
-                    stop: mean + 4.0 * sd,
+                    start: mean - distance_to_value_of_one,
+                    stop: mean + distance_to_value_of_one,
                     mean,
                     sd,
-                    peak_amplitude: height.sample(frame)?,
+                    peak_amplitude,
                 })
-            }
-            PulseTemplate::Biexp {
-                start,
+            },
+            PulseTemplate::BackToBackExp {
+                peak_height,
+                peak_time,
+                spread,
                 decay,
                 rise,
-                height,
             } => {
-                let start = start.sample(frame)?;
-                let decay = decay.sample(frame)?;
-                let rise = rise.sample(frame)?;
-                let peak_height = height.sample(frame)?;
-                let ratio = decay / rise;
-                let coef = peak_height
-                    / (f64::powf(ratio, 1.0 / ratio - 1.0) - f64::powf(ratio, 1.0 - ratio));
-                let peak_time = f64::ln(f64::powf(ratio, decay * rise / (decay - rise)));
-                let stop = f64::MAX; // This needs to be changed
-                Ok(Self::Biexp {
+                let rising = rise.sample(frame)?;
+                let falling = decay.sample(frame)?;
+                let peak_height = peak_height.sample(frame)?;
+                let spread = spread.sample(frame)?;
+                let peak_time = peak_time.sample(frame)?;
+
+                let rising_spread =  rising * spread.powi(2);
+                let falling_spread = falling * spread.powi(2);
+                let frac_1_sqrt_2_spread = std::f64::consts::FRAC_1_SQRT_2 / spread;
+
+                let normalising_factor = {
+                    let rising_exp = f64::exp(rising * (0.5 * rising_spread));
+                    let rising_erfc =
+                        libm::erfc(rising_spread * frac_1_sqrt_2_spread);
+                    let falling_exp = f64::exp(falling * (0.5 * falling_spread));
+                    let falling_erfc =
+                        libm::erfc(falling_spread * frac_1_sqrt_2_spread);
+
+                    peak_height/(rising_exp * rising_erfc + falling_exp * falling_erfc)
+                };
+
+                let start = peak_time - 0.5 * rising_spread - normalising_factor.ln()/rising;
+                let stop = peak_time + 0.5 * falling_spread + normalising_factor.ln()/falling;
+
+                Ok(Self::BackToBackExp {
                     start,
                     stop,
-                    decay,
-                    rise,
-                    peak_height,
-                    coef,
                     peak_time,
+                    falling,
+                    rising,
+                    normalising_factor,
+                    rising_spread,
+                    falling_spread,
+                    frac_1_sqrt_2_spread: std::f64::consts::FRAC_1_SQRT_2 / spread,
                 })
             }
         }
@@ -138,7 +163,7 @@ impl PulseEvent {
             Self::Flat { start, .. } => *start,
             Self::Triangular { start, .. } => *start,
             Self::Gaussian { start, .. } => *start,
-            Self::Biexp { start, .. } => *start,
+            Self::BackToBackExp { start, .. } => *start,
         }) as Time
     }
 
@@ -147,7 +172,7 @@ impl PulseEvent {
             Self::Flat { stop, .. } => *stop,
             Self::Triangular { stop, .. } => *stop,
             Self::Gaussian { stop, .. } => *stop,
-            Self::Biexp { stop, .. } => *stop,
+            Self::BackToBackExp { stop, .. } => *stop,
         }) as Time
     }
 
@@ -156,19 +181,28 @@ impl PulseEvent {
             Self::Flat { start, .. } => *start,
             Self::Triangular { peak_time, .. } => *peak_time,
             Self::Gaussian { mean, .. } => *mean,
-            Self::Biexp {
+            Self::BackToBackExp {
                 start, peak_time, ..
             } => *start + *peak_time / 2.0,
         }) as Time
     }
 
     pub(crate) fn intensity(&self) -> Intensity {
-        *match self {
-            Self::Flat { amplitude, .. } => amplitude,
-            Self::Triangular { amplitude, .. } => amplitude,
-            Self::Gaussian { peak_amplitude, .. } => peak_amplitude,
-            Self::Biexp { peak_height, .. } => peak_height,
-        } as Intensity
+        (match self {
+            Self::Flat { amplitude, .. } => *amplitude,
+            Self::Triangular { amplitude, .. } => *amplitude,
+            Self::Gaussian { peak_amplitude, .. } => *peak_amplitude,
+            Self::BackToBackExp { falling, rising, normalising_factor, rising_spread, falling_spread, frac_1_sqrt_2_spread, .. } => {
+                    let rising_exp = f64::exp(rising * (0.5 * rising_spread));
+                    let rising_erfc =
+                        libm::erfc(rising_spread * frac_1_sqrt_2_spread);
+                    let falling_exp = f64::exp(falling * (0.5 * falling_spread));
+                    let falling_erfc =
+                        libm::erfc(falling_spread * frac_1_sqrt_2_spread);
+
+                    normalising_factor * (rising_exp * rising_erfc + falling_exp * falling_erfc)
+            },
+        }) as Intensity
     }
 
     pub(crate) fn get_value_at(&self, time: f64) -> f64 {
@@ -202,26 +236,39 @@ impl PulseEvent {
                 mean,
                 sd,
                 peak_amplitude,
-                ..
+                start,
+                stop
             } => {
-                if mean - 6.0 * sd > time || time > mean + 6.0 * sd {
+                if start > time || time > stop {
                     f64::default()
                 } else {
                     peak_amplitude * f64::exp(-f64::powi(0.5 * (time - mean) / sd, 2))
                 }
             }
-            Self::Biexp {
+            Self::BackToBackExp {
                 start,
-                decay,
-                rise,
-                coef,
-                ..
+                stop,
+                peak_time,
+                falling,
+                rising,
+                normalising_factor,
+                rising_spread,
+                falling_spread,
+                frac_1_sqrt_2_spread,
             } => {
-                if time < start {
-                    f64::default()
+                if start < time || time < stop {
+                    let time_shift = time - peak_time;
+
+                    let rising_exp = f64::exp(rising * (0.5 * rising_spread + time_shift));
+                    let rising_erfc =
+                        libm::erfc((rising_spread + time_shift) * frac_1_sqrt_2_spread);
+                    let falling_exp = f64::exp(falling * (0.5 * falling_spread - time_shift));
+                    let falling_erfc =
+                        libm::erfc((falling_spread - time_shift) * frac_1_sqrt_2_spread);
+                    
+                    normalising_factor * (rising_exp * rising_erfc + falling_exp * falling_erfc)
                 } else {
-                    let time = time - start;
-                    coef * (f64::exp(-time / decay) - f64::exp(-time / rise))
+                    Default::default()
                 }
             }
         }
