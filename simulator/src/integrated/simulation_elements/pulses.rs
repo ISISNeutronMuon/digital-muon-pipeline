2d5d06c7a95112077ff2f46708011b1d6070769b
Modularius <daniel.kirk@rocketmail.com>
2026-02-12 13:06:47 +0000

    Added B2B Pu;se

--- simulator/src/integrated/simulation_elements/pulses.rs
+++ simulator/src/integrated/simulation_elements/pulses.rs
@@ -103,7 +103,7 @@ impl PulseEvent {
                 let mean = peak_time.sample(frame)?;
                 let sd = sd.sample(frame)?;
                 let peak_amplitude = height.sample(frame)?;
-                let distance_to_value_of_one = 2.0*sd*peak_amplitude.ln().sqrt();
+                let distance_to_value_of_one = 2.0 * sd * peak_amplitude.ln().sqrt();
                 Ok(Self::Gaussian {
                     start: mean - distance_to_value_of_one,
                     stop: mean + distance_to_value_of_one,
@@ -111,7 +111,7 @@ impl PulseEvent {
                     sd,
                     peak_amplitude,
                 })
-            },
+            }
             PulseTemplate::BackToBackExp {
                 peak_height,
                 peak_time,
@@ -125,23 +125,29 @@ impl PulseEvent {
                 let spread = spread.sample(frame)?;
                 let peak_time = peak_time.sample(frame)?;
 
-                let rising_spread =  rising * spread.powi(2);
+                let rising_spread = rising * spread.powi(2);
                 let falling_spread = falling * spread.powi(2);
                 let frac_1_sqrt_2_spread = std::f64::consts::FRAC_1_SQRT_2 / spread;
 
                 let normalising_factor = {
-                    let rising_erfc =
-                        libm::erfc(rising_spread * frac_1_sqrt_2_spread);
-                    let rising_exp = if rising_erfc == 0.0 { 0.0 } else { f64::exp(0.5*rising*rising_spread) };
-                    let falling_erfc =
-                        libm::erfc(falling_spread * frac_1_sqrt_2_spread);
-                    let falling_exp = if falling_erfc == 0.0 { 0.0 } else { f64::exp(0.5*falling*falling_spread) };
+                    let rising_erfc = libm::erfc(rising_spread * frac_1_sqrt_2_spread);
+                    let rising_exp = if rising_erfc == 0.0 {
+                        0.0
+                    } else {
+                        f64::exp(0.5 * rising * rising_spread)
+                    };
+                    let falling_erfc = libm::erfc(falling_spread * frac_1_sqrt_2_spread);
+                    let falling_exp = if falling_erfc == 0.0 {
+                        0.0
+                    } else {
+                        f64::exp(0.5 * falling * falling_spread)
+                    };
 
-                    peak_height/(rising_exp * rising_erfc + falling_exp * falling_erfc)
+                    peak_height / (rising_exp * rising_erfc + falling_exp * falling_erfc)
                 };
 
-                let start = peak_time - 0.5 * rising_spread - normalising_factor.ln()/rising;
-                let stop = peak_time + 0.5 * falling_spread + normalising_factor.ln()/falling;
+                let start = peak_time - 0.5 * rising_spread - normalising_factor.ln() / rising;
+                let stop = peak_time + 0.5 * falling_spread + normalising_factor.ln() / falling;
 
                 Ok(Self::BackToBackExp {
                     start,
@@ -190,16 +196,22 @@ impl PulseEvent {
             Self::Flat { amplitude, .. } => *amplitude,
             Self::Triangular { amplitude, .. } => *amplitude,
             Self::Gaussian { peak_amplitude, .. } => *peak_amplitude,
-            Self::BackToBackExp { falling, rising, normalising_factor, rising_spread, falling_spread, frac_1_sqrt_2_spread, .. } => {
+            Self::BackToBackExp {
+                falling,
+                rising,
+                normalising_factor,
+                rising_spread,
+                falling_spread,
+                frac_1_sqrt_2_spread,
+                ..
+            } => {
                 let rising_exp = f64::exp(rising * (0.5 * rising_spread));
-                let rising_erfc =
-                    libm::erfc(rising_spread * frac_1_sqrt_2_spread);
+                let rising_erfc = libm::erfc(rising_spread * frac_1_sqrt_2_spread);
                 let falling_exp = f64::exp(falling * (0.5 * falling_spread));
-                let falling_erfc =
-                    libm::erfc(falling_spread * frac_1_sqrt_2_spread);
+                let falling_erfc = libm::erfc(falling_spread * frac_1_sqrt_2_spread);
 
                 normalising_factor * (rising_exp * rising_erfc + falling_exp * falling_erfc)
-            },
+            }
         }) as Intensity
     }
 
@@ -207,7 +219,7 @@ impl PulseEvent {
         if self.get_start() > time as Time || time as Time > self.get_end() {
             return Default::default();
         }
-        
+
         match *self {
             Self::Flat { amplitude, .. } => amplitude,
             Self::Triangular {
@@ -240,17 +252,21 @@ impl PulseEvent {
             } => {
                 let time_shift = time - peak_time;
 
-                let rising_erfc =
-                    libm::erfc((rising_spread + time_shift) * frac_1_sqrt_2_spread);
-                let rising_exp = (rising_erfc != 0.0)   //  Guard against NaN
-                    .then_some(f64::exp(rising * (0.5 * rising_spread + time_shift)))
-                    .unwrap_or_default();
+                let rising_erfc = libm::erfc((rising_spread + time_shift) * frac_1_sqrt_2_spread);
+                //  Guard against rising_exp being NaN
+                let rising_exp = if rising_erfc != 0.0 {
+                    f64::exp(rising * (0.5 * rising_spread + time_shift))
+                } else {
+                    Default::default()
+                };
 
-                let falling_erfc =
-                    libm::erfc((falling_spread - time_shift) * frac_1_sqrt_2_spread);
-                let falling_exp = (falling_erfc != 0.0)   //  Guard against NaN
-                    .then_some(f64::exp(falling * (0.5 * falling_spread - time_shift)))
-                    .unwrap_or_default();
+                let falling_erfc = libm::erfc((falling_spread - time_shift) * frac_1_sqrt_2_spread);
+                //  Guard against falling_exp being NaN
+                let falling_exp = if falling_erfc != 0.0 {
+                    f64::exp(falling * (0.5 * falling_spread - time_shift))
+                } else {
+                    Default::default()
+                };
 
                 normalising_factor * (rising_exp * rising_erfc + falling_exp * falling_erfc)
             }
@@ -263,13 +279,23 @@ mod tests {
     use crate::integrated::simulation_elements::NumExpression;
 
     use super::*;
-    
-    const TEMPLATE : PulseTemplate = PulseTemplate::BackToBackExp {
-        peak_height: FloatRandomDistribution::ConstantFloat { value: NumExpression::Const(2100.0) },
-        peak_time: FloatRandomDistribution::ConstantFloat { value: NumExpression::Const(2200.0) },
-        spread: FloatRandomDistribution::ConstantFloat { value: NumExpression::Const(3.0) },
-        falling: FloatRandomDistribution::ConstantFloat { value: NumExpression::Const(2.5) },
-        rising: FloatRandomDistribution::ConstantFloat { value: NumExpression::Const(1.5) },
+
+    const TEMPLATE: PulseTemplate = PulseTemplate::BackToBackExp {
+        peak_height: FloatRandomDistribution::ConstantFloat {
+            value: NumExpression::Const(2100.0),
+        },
+        peak_time: FloatRandomDistribution::ConstantFloat {
+            value: NumExpression::Const(2200.0),
+        },
+        spread: FloatRandomDistribution::ConstantFloat {
+            value: NumExpression::Const(3.0),
+        },
+        falling: FloatRandomDistribution::ConstantFloat {
+            value: NumExpression::Const(2.5),
+        },
+        rising: FloatRandomDistribution::ConstantFloat {
+            value: NumExpression::Const(1.5),
+        },
     };
 
     #[test]
@@ -282,14 +308,19 @@ mod tests {
         assert_eq!(pulse.intensity(), 2100);
         assert_eq!(pulse.time(), 2200);
     }
-    
 
     #[test]
     fn back_to_back_exp_values() {
         let pulse = PulseEvent::sample(&TEMPLATE, 0).unwrap();
-        const VALUES : [Intensity; 27] = [0, 1, 5, 16, 41, 95, 199, 379, 651, 1011, 1418, 1793, 2044, 2100, 1942, 1616, 1211, 816, 495, 270, 132, 58, 23, 8, 2, 0, 0];
+        const VALUES: [Intensity; 27] = [
+            0, 1, 5, 16, 41, 95, 199, 379, 651, 1011, 1418, 1793, 2044, 2100, 1942, 1616, 1211,
+            816, 495, 270, 132, 58, 23, 8, 2, 0, 0,
+        ];
         for (t, &v) in VALUES.iter().enumerate() {
-            assert_eq!(pulse.get_value_at((pulse.get_start() + t as Time) as f64) as Intensity, v);
+            assert_eq!(
+                pulse.get_value_at((pulse.get_start() + t as Time) as f64) as Intensity,
+                v
+            );
         }
     }
 }
