7d157f82d304ed0bb6c407765dcf951d1498ecf4
Modularius <daniel.kirk@rocketmail.com>
2026-03-13 17:30:00 +0000

    Fixes

--- trace-to-events/src/pulse_detection/iterators/mod.rs
+++ trace-to-events/src/pulse_detection/iterators/mod.rs
@@ -1,8 +1,9 @@
 //! Provides iterators to convert raw trace data into events and pulses.
-//pub(crate) mod assembler;
 pub(crate) mod event;
+pub(crate) mod padding;
 pub(crate) mod window;
 
 use super::{Detector, TracePoint};
 pub(crate) use event::EventsIterable;
+pub(crate) use padding::PaddingIterable;
 pub(crate) use window::WindowIterable;
