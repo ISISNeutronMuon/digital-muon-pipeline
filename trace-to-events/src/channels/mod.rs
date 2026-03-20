911e2c5d1165e39d309b9c7934293e0a6bcef147
Modularius <daniel.kirk@rocketmail.com>
2026-03-20 12:19:37 +0000

    Refactor channels.rs into module folder

--- trace-to-events/src/channels/mod.rs
+++ trace-to-events/src/channels/mod.rs
@@ -1,23 +1,19 @@
 //! Provides functions and structs which extract and return lists of muon events using specified detectors and settings.
+mod algorithms;
 use crate::{
+    channels::algorithms::{
+        find_differential_threshold_events, find_fixed_threshold_events, find_smoothing_events,
+    },
     parameters::{
         DetectorSettings, Mode, PeakHeightBasis, PeakHeightMode, Polarity,
         SmoothingDetectorParameters,
     },
     pulse_detection::{
-        EventsIterable, Real, WindowIterable,
-        detectors::{
-            differential_threshold_detector::{
-                DifferentialThresholdDetector, DifferentialThresholdParameters,
-            },
-            local_arg_min_detector::LocalArgMinDetector,
-            region_detector::RegionDetector,
-        },
-        iterators::PaddingIterable,
-        threshold_detector::{ThresholdDetector, ThresholdDuration},
-        utils::stddev_from_slice,
+        Real,
+        detectors::differential_threshold_detector::DifferentialThresholdParameters,
+        threshold_detector::ThresholdDuration,
         window::{
-            FiniteDifferences, SliceWindow,
+            FiniteDifferences,
             convolution_filter::{ConvolutionFilter, KernelType},
         },
     },
@@ -63,7 +59,6 @@ struct SmoothingAlgorithmParameters {
 struct SmoothingDetectorCache {
     time: Vec<Real>,
     input_values: Vec<Real>,
-    input_values2: Vec<Real>,
     output_values: Vec<Real>,
 }
 
@@ -76,7 +71,6 @@ impl SmoothingDetectorCache {
         // FIXME: Should there be some sort of check for absurdly big trace sizes?
         if input_size > self.input_values.len() {
             self.input_values.resize(input_size, Default::default());
-            self.input_values2.resize(input_size, Default::default());
         }
 
         if output_size > self.output_values.len() {
@@ -219,157 +213,4 @@ impl ChannelProcessor {
         tracing::Span::current().record("num_pulses", result.0.len());
         result
     }
-}
-
-/// Extract muon events from the given trace, using the fixed threshold discriminator and the given settings.
-/// # Parameters
-/// - trace: raw trace data.
-/// - sample_time: sample time in ns.
-/// - polarity: the polarity of the trace signal.
-/// - baseline: the baseline of the trace signal.
-/// - parameters: settings to use for the fixed threshold discriminator.
-#[tracing::instrument(skip_all, level = "trace")]
-fn find_fixed_threshold_events(
-    trace: &ChannelTrace,
-    sample_time: Real,
-    polarity_sign: Real,
-    baseline: Real,
-    parameters: &ThresholdDuration,
-) -> (Vec<Time>, Vec<Intensity>) {
-    let raw = trace
-        .voltage()
-        .unwrap()
-        .into_iter()
-        .enumerate()
-        .map(|(i, v)| {
-            (
-                i as Real * sample_time,
-                polarity_sign * (v as Real - baseline),
-            )
-        });
-
-    let pulses = raw.clone().events(ThresholdDetector::new(parameters));
-
-    let mut time = Vec::<Time>::new();
-    let mut voltage = Vec::<Intensity>::new();
-    for pulse in pulses {
-        time.push(pulse.0 as Time);
-        voltage.push(pulse.1.pulse_height as Intensity);
-    }
-    (time, voltage)
-}
-
-/// Extract muon events from the given trace, using the differential threshold detector and the given settings.
-/// # Parameters
-/// - trace: raw trace data.
-/// - sample_time: sample time in ns.
-/// - polarity: the polarity of the trace signal.
-/// - baseline: the baseline of the trace signal.
-/// - parameters: settings to use for the differential threshold detector.
-#[tracing::instrument(skip_all, level = "trace")]
-fn find_differential_threshold_events(
-    trace: &ChannelTrace,
-    sample_time: Real,
-    polarity_sign: Real,
-    baseline: Real,
-    finite_differences: &FiniteDifferences<2>,
-    parameters: &DifferentialThresholdParameters,
-    peak_height: &PeakHeightParameters,
-) -> (Vec<Time>, Vec<Intensity>) {
-    let raw = trace
-        .voltage()
-        .unwrap()
-        .into_iter()
-        .enumerate()
-        .map(|(i, v)| {
-            (
-                i as Real * sample_time,
-                polarity_sign * (v as Real - baseline),
-            )
-        });
-
-    let pulses = raw
-        .clone()
-        .window(finite_differences.clone_only_coefficients())
-        .events(DifferentialThresholdDetector::new(
-            parameters,
-            peak_height.mode.clone(),
-        ));
-
-    let mut time = Vec::<Time>::new();
-    let mut voltage = Vec::<Intensity>::new();
-    for pulse in pulses {
-        time.push(pulse.0 as Time);
-        voltage.push(match peak_height.basis {
-            PeakHeightBasis::TraceBaseline => pulse.1.peak_height as Intensity,
-            PeakHeightBasis::PulseBaseline => {
-                (pulse.1.peak_height - pulse.1.base_height) as Intensity
-            }
-        });
-    }
-    (time, voltage)
-}
-
-#[tracing::instrument(skip_all, level = "trace")]
-fn find_smoothing_events(
-    trace: &ChannelTrace,
-    fin_diff_gaussian: &ConvolutionFilter,
-    cache: &mut SmoothingDetectorCache,
-    sample_time: Real,
-    polarity_sign: Real,
-    baseline: Real,
-    parameters: &SmoothingDetectorParameters,
-) -> (Vec<Time>, Vec<Intensity>) {
-    let raw_voltages = trace
-        .voltage()
-        .expect("Trace voltage should be Some, this should never fail.");
-
-    cache.ensure_time_data_written((0..raw_voltages.len()).map(|t| (t as Real) * sample_time));
-    let kernel_radius = fin_diff_gaussian.kernel_size() >> 1;
-    let padded = raw_voltages
-        .iter()
-        .map(|v| polarity_sign * (v as Real - baseline))
-        .pad_reflect(kernel_radius, kernel_radius);
-    cache.ensure_cache_lengths(
-        raw_voltages.len() + fin_diff_gaussian.kernel_size(),
-        raw_voltages.len(),
-    );
-    cache.write_input_values(padded);
-
-    fin_diff_gaussian.apply_to_slice(
-        cache.input_values.as_slice(),
-        cache.output_values.as_mut_slice(),
-    );
-    //let mid_slice = &mut cache.input_values2[0..cache.input_values.len() - fin_diff.kernel_size()];
-    //fin_diff.apply_to_slice(cache.input_values.as_slice(), mid_slice);
-    //gaussian.apply_to_slice(mid_slice, cache.output_values.as_mut_slice());
-
-    let percentile = ((raw_voltages.len() as f64 * parameters.noise_centile) / 100.0) as usize;
-    let noise_std = stddev_from_slice(&cache.output_values[percentile..])
-        .expect("StdDev should exist, this should never fail.");
-
-    let output_iter = cache.output_values.iter().cloned().enumerate();
-
-    let regions = output_iter.clone().events(RegionDetector::new(
-        -noise_std * parameters.nsig_noise,
-        parameters.min_size,
-    ));
-    let pulses = regions
-        .flat_map(|region| {
-            output_iter
-                .clone()
-                .take(region.1)
-                .skip(region.0)
-                .events(LocalArgMinDetector::default())
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
-
-    let mut times = Vec::<Time>::new();
-    let mut voltages = Vec::<Intensity>::new();
-    for time in pulses {
-        times.push(time as Time);
-        voltages.push(raw_voltages.get(time));
-    }
-    (times, voltages)
 }
