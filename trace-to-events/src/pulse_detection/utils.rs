17caf216a76fbf3128402e25a4f6ec973023632e
Modularius <daniel.kirk@rocketmail.com>
2026-03-23 22:33:07 +0000

    Add utils and fix up pulse detection

---
+++ trace-to-events/src/pulse_detection/utils.rs
@@ -0,0 +1,15 @@
+use crate::pulse_detection::Real;
+
+#[tracing::instrument(level = "trace", skip_all)]
+pub(crate) fn std_dev(v: &[Real]) -> Result<Real, &'static str> {
+    let len = v.len();
+    if len == 0 {
+        Err("Cannot compute standard deviation")
+    } else if len == 1 {
+        Ok(0.0)
+    } else {
+        let mean = v.iter().sum::<Real>() / len as Real;
+        let var = v.iter().map(|x| (x - mean).powi(2)).sum::<Real>() / (len - 1) as Real;
+        Ok(var.sqrt())
+    }
+}
