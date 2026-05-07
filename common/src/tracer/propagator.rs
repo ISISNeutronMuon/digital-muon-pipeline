STFC-ICD-Research-and-Design/supermusr-data-pipeline

commit 3e2e9a4810e7d3c691a49f57a9965b585acd37f7

Consolidate tracing span data (#333)
## Summary of changes

- Moved `kafka_message_timestamp_ms` span field from
`process_kafka_message` to `process_digitiser_event_list_message` and
`process_digitiser_trace_message` in the aggregator and event formation
components respectively.
- Changed level of `process_kafka_message` to `debug` in both
components.
- Standardised (not exhaustively) some variable names in
`process_digitiser_event_list_message` and
`process_digitiser_trace_message`.

## Instruction for review/testing

General code review.
Has been tested on simulated data.

digitiser-aggregator/src/main.rs
@@ -194,14 +194,14 @@ async fn main() -> anyhow::Result<()> {
 }
 
 ///  This function wraps the `root_as_digitizer_event_list_message` function, allowing it to be instrumented.
-#[instrument(skip_all, level = "trace", err(level = "WARN"))]
+#[instrument(skip_all, level = "trace", err(level = "warn"))]
 fn spanned_root_as_digitizer_event_list_message(
     payload: &[u8],
 ) -> Result<DigitizerEventListMessage<'_>, InvalidFlatbuffer> {
     root_as_digitizer_event_list_message(payload)
 }
 
-#[instrument(skip_all, fields(kafka_message_timestamp_ms = msg.timestamp().to_millis()))]
+#[instrument(skip_all, level = "debug", err(level = "warn"))]
 async fn process_kafka_message(
     use_otel: bool,
     channel_send: &AggregatedFrameToBufferSender,
@@ -218,8 +218,15 @@ async fn process_kafka_message(
             )
             .increment(1);
             match spanned_root_as_digitizer_event_list_message(payload) {
-                Ok(msg) => {
-                    process_digitiser_event_list_message(channel_send, cache, msg).await?;
+                Ok(data) => {
+                    let kafka_timestamp_ms = msg.timestamp().to_millis().unwrap_or(-1);
+                    process_digitiser_event_list_message(
+                        channel_send,
+                        cache,
+                        kafka_timestamp_ms,
+                        data,
+                    )
+                    .await?;
                 }
                 Err(e) => {
                     warn!("Failed to parse message: {}", e);
@@ -245,7 +252,8 @@ async fn process_kafka_message(
 }
 
 #[tracing::instrument(skip_all, fields(
-    digitiser_id = msg.digitizer_id(),
+    digitiser_id = message.digitizer_id(),
+    kafka_message_timestamp_ms=kafka_message_timestamp_ms,
     metadata_timestamp,
     metadata_frame_number,
     metadata_period_number,
@@ -259,14 +267,15 @@ async fn process_kafka_message(
 async fn process_digitiser_event_list_message(
     channel_send: &AggregatedFrameToBufferSender,
     cache: &mut FrameCache<EventData>,
-    msg: DigitizerEventListMessage<'_>,
+    kafka_message_timestamp_ms: i64,
+    message: DigitizerEventListMessage<'_>,
 ) -> Result<(), SendAggregatedFrameError> {
-    match msg.metadata().try_into() {
+    match message.metadata().try_into() {
         Ok(metadata) => {
-            debug!("Event packet: metadata: {:?}", msg.metadata());
+            debug!("Event packet: metadata: {:?}", message.metadata());
 
             // Push the current digitiser message to the frame cache, possibly creating a new partial frame
-            if let Err(err) = cache.push(msg.digitizer_id(), &metadata, msg.into()) {
+            if let Err(err) = cache.push(message.digitizer_id(), &metadata, message.into()) {
                 tracing::Span::current().record(err.into(), true);
             }
 

trace-to-events/src/main.rs
@@ -181,14 +181,14 @@ async fn main() -> anyhow::Result<()> {
     }
 }
 
-#[instrument(skip_all, level = "trace", err(level = "WARN"))]
+#[instrument(skip_all, level = "trace", err(level = "warn"))]
 fn spanned_root_as_digitizer_analog_trace_message(
     payload: &[u8],
 ) -> Result<DigitizerAnalogTraceMessage<'_>, InvalidFlatbuffer> {
     root_as_digitizer_analog_trace_message(payload)
 }
 
-#[instrument(skip_all, fields(kafka_message_timestamp_ms = m.timestamp().to_millis()))]
+#[instrument(skip_all, level = "debug", err(level = "warn"))]
 fn process_kafka_message(
     tracer: &TracerEngine,
     args: &Cli,
@@ -213,14 +213,18 @@ fn process_kafka_message(
             )
             .increment(1);
             match spanned_root_as_digitizer_analog_trace_message(payload) {
-                Ok(thing) => process_digitiser_trace_message(
-                    tracer,
-                    m.headers(),
-                    args,
-                    sender,
-                    producer,
-                    thing,
-                )?,
+                Ok(data) => {
+                    let kafka_timestamp_ms = m.timestamp().to_millis().unwrap_or(-1);
+                    process_digitiser_trace_message(
+                        tracer,
+                        m.headers(),
+                        args,
+                        sender,
+                        producer,
+                        kafka_timestamp_ms,
+                        data,
+                    )?
+                }
                 Err(e) => {
                     warn!("Failed to parse message: {}", e);
                     counter!(
@@ -245,13 +249,14 @@ fn process_kafka_message(
 #[instrument(
     skip_all,
     fields(
-        digitiser_id = thing.digitizer_id(),
-        metadata_timestamp = tracing::field::Empty,
-        metadata_frame_number = tracing::field::Empty,
-        metadata_period_number = tracing::field::Empty,
-        metadata_veto_flags = tracing::field::Empty,
-        metadata_protons_per_pulse = tracing::field::Empty,
-        metadata_running = tracing::field::Empty,
+        digitiser_id = message.digitizer_id(),
+        kafka_message_timestamp_ms = kafka_timestamp_ms,
+        metadata_timestamp,
+        metadata_frame_number,
+        metadata_period_number,
+        metadata_veto_flags,
+        metadata_protons_per_pulse,
+        metadata_running,
     )
 )]
 fn process_digitiser_trace_message(
@@ -260,12 +265,13 @@ fn process_digitiser_trace_message(
     args: &Cli,
     sender: &DigitiserEventListToBufferSender,
     producer: &FutureProducer,
-    thing: DigitizerAnalogTraceMessage,
+    kafka_timestamp_ms: i64,
+    message: DigitizerAnalogTraceMessage,
 ) -> Result<(), TrySendDigitiserEventListError> {
-    thing
+    message
         .metadata()
         .try_into()
-        .map(|metadata: FrameMetadata| {
+        .inspect(|metadata: &FrameMetadata| {
             record_metadata_fields_to_span!(metadata, tracing::Span::current());
         })
         .ok();
@@ -274,7 +280,7 @@ fn process_digitiser_trace_message(
     let mut fbb = FlatBufferBuilder::new();
     processing::process(
         &mut fbb,
-        &thing,
+        &message,
         &DetectorSettings {
             polarity: &args.polarity,
             baseline: args.baseline,

ISISNeutronMuon/digital-muon-pipeline

commit 7388894ae0294aa0db9a0d29e6270c721809189d

Fix inter-processes traces (#488)
## Summary of changes

~~Revert change made in commit
https://github.com/ISISNeutronMuon/digital-muon-pipeline/commit/3e2e9a4810e7d3c691a49f57a9965b585acd37f7,
which broke tracing across process boundaries.~~

A new version of OpenTelemetry changed the way traces are linked across
process boundaries. Code changed in all three components and the common
crate to reflect this.

## Instruction for review/testing

General code review.
Has been tested with simulated data.

common/src/tracer/propagator.rs
@@ -87,15 +87,10 @@ impl FutureRecordTracerExt for FutureRecord<'_, str, [u8]> {
 /// indicating whether OpenTelemetry is used.
 /// If this is false, the methods usually do nothing.
 pub trait OptionalHeaderTracerExt {
-    fn conditional_extract_to_current_span(self, use_otel: bool);
     fn conditional_extract_to_span(self, use_otel: bool, span: &Span);
 }
 
 impl OptionalHeaderTracerExt for Option<&BorrowedHeaders> {
-    fn conditional_extract_to_current_span(self, use_otel: bool) {
-        self.conditional_extract_to_span(use_otel, &tracing::Span::current())
-    }
-
     fn conditional_extract_to_span(self, use_otel: bool, span: &Span) {
         if let Some(headers) = self
             && use_otel

digitiser-aggregator/src/main.rs
@@ -217,7 +217,14 @@ async fn main() -> miette::Result<()> {
             event = consumer.recv() => {
                 match event {
                     Ok(msg) => {
-                        process_kafka_message(tracer.use_otel(), &channel_send, &mut cache, &msg).await.into_diagnostic().wrap_err("Failed to process incomming message")?;
+                        let span = info_span!("message_received");
+                        msg.headers().conditional_extract_to_span(tracer.use_otel(), &span);
+                        let _guard = span.enter();
+                        process_kafka_message(&channel_send, &mut cache, &msg)
+                            .await
+                            .into_diagnostic()
+                            .wrap_err("Failed to process incomming message")?;
+
                         consumer.commit_message(&msg, CommitMode::Async)
                             .expect("Message should commit");
                     }
@@ -247,21 +254,17 @@ fn spanned_root_as_digitizer_event_list_message(
 
 /// Extracts the payload of a Kafka message and passes it to [process_digitiser_event_list_message]
 /// # Parameters
-/// - use_otel: if true, then attempts to extract a parent [Span] from the Kafka headers.
 /// - channel_send: send channel which takes [AggregatedFrame] objects to dispatch.
 /// - cache: the cache in which frames are stored whilst awaiting digitiser messages.
 /// - msg: the message.
 ///
 /// [Span]: tracing::Span
-#[instrument(skip_all, level = "debug", err(level = "warn"))]
+#[instrument(skip_all, level = "info", err(level = "warn"))]
 async fn process_kafka_message(
-    use_otel: bool,
     channel_send: &AggregatedFrameToBufferSender,
     cache: &mut FrameCache<EventData>,
     msg: &BorrowedMessage<'_>,
 ) -> Result<(), SendAggregatedFrameError> {
-    msg.headers().conditional_extract_to_current_span(use_otel);
-
     if let Some(payload) = msg.payload() {
         if digitizer_event_list_message_buffer_has_identifier(payload) {
             counter!(

nexus-writer/src/main.rs
@@ -53,7 +53,7 @@ use tokio::{
     signal::unix::{SignalKind, signal},
     time,
 };
-use tracing::{debug, error, warn};
+use tracing::{debug, error, info_span, warn};
 
 /// [clap] derived struct to handle command line parameters.
 #[derive(Debug, Parser)]
@@ -248,9 +248,13 @@ async fn main() -> miette::Result<()> {
                         warn!("{e}")
                     },
                     Ok(msg) => {
-                        process_kafka_message(&topics, &mut nexus_engine, tracer.use_otel(), &msg);
+                        let span = info_span!("message_received");
+                        msg.headers().conditional_extract_to_span(tracer.use_otel(), &span);
+                        let _guard = span.enter();
 
-                        if let Err(e) = consumer.commit_message(&msg, CommitMode::Async){
+                        process_kafka_message(&topics, &mut nexus_engine, &msg);
+
+                        if let Err(e) = consumer.commit_message(&msg, CommitMode::Async) {
                             error!("Failed to commit Kafka message consumption: {e}");
                         }
                     }
@@ -273,22 +277,19 @@ async fn main() -> miette::Result<()> {
 /// # Parameters
 /// - topics: contains the topic names.
 /// - nexus_engine: the engine to push the message to.
-/// - use_otel: if true, then attempts to extract a parent [Span] from the Kafka headers.
 /// - msg: the message.
 ///
 /// [Span]: tracing::Span
-#[tracing::instrument(skip_all, level="debug", fields(
+#[tracing::instrument(skip_all, level="info",
+    fields(
     num_cached_runs = nexus_engine.get_num_cached_runs(),
     kafka_message_timestamp_ms = msg.timestamp().to_millis()
 ))]
 fn process_kafka_message(
     topics: &Topics,
     nexus_engine: &mut NexusEngine<EngineDependencies>,
-    use_otel: bool,
     msg: &BorrowedMessage,
 ) {
-    msg.headers().conditional_extract_to_current_span(use_otel);
-
     debug!(
         "key: '{:?}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
         msg.key(),

trace-to-events/src/main.rs
@@ -46,7 +46,7 @@ use parameters::{DetectorSettings, Mode, Polarity};
 use rdkafka::{
     Message,
     consumer::{CommitMode, Consumer},
-    message::{BorrowedHeaders, BorrowedMessage},
+    message::BorrowedMessage,
     producer::{DeliveryFuture, FutureProducer, FutureRecord},
 };
 use std::net::SocketAddr;
@@ -56,7 +56,7 @@ use tokio::{
     sync::mpsc::{Receiver, Sender, error::TrySendError},
     task::JoinHandle,
 };
-use tracing::{debug, error, info, instrument, trace, warn};
+use tracing::{debug, error, info, info_span, instrument, trace, warn};
 
 type InstrumentedDeliveryFuture = tracing::instrument::Instrumented<DeliveryFuture>;
 type DigitiserEventListToBufferSender = Sender<InstrumentedDeliveryFuture>;
@@ -207,12 +207,16 @@ async fn main() -> miette::Result<()> {
         tokio::select! {
             msg = consumer.recv() => match msg {
                 Ok(m) => {
+                    let span = info_span!("message_received");
+                    m.headers().conditional_extract_to_span(tracer.use_otel(), &span);
+                    let _guard = span.enter();
                     process_kafka_message(
                         &tracer,
                         &sender_parameters,
                         &mut message_processor,
                         &m,
                     ).into_diagnostic()?;
+
                     consumer.commit_message(&m, CommitMode::Async).unwrap();
                 }
                 Err(e) => warn!("Kafka error: {}", e)
@@ -244,7 +248,7 @@ fn spanned_root_as_digitizer_analog_trace_message(
 /// - m: the message.
 ///
 /// [Span]: tracing::Span
-#[instrument(skip_all, level = "debug", err(level = "warn"))]
+#[instrument(skip_all, level = "info", err(level = "warn"))]
 fn process_kafka_message(
     tracer: &TracerEngine,
     sender_parameters: &SenderParameters,
@@ -268,7 +272,6 @@ fn process_kafka_message(
                     process_digitiser_trace_message(
                         tracer,
                         kafka_timestamp_ms,
-                        message.headers(),
                         sender_parameters,
                         message_processor,
                         trace_message,
@@ -321,7 +324,6 @@ fn process_kafka_message(
 fn process_digitiser_trace_message(
     tracer: &TracerEngine,
     kafka_timestamp_ms: i64,
-    headers: Option<&BorrowedHeaders>,
     sender_parameters: &SenderParameters,
     message_processor: &mut DigitiserMessageProcessor,
     message: DigitizerAnalogTraceMessage,
@@ -376,8 +378,7 @@ fn process_digitiser_trace_message(
         })
         .ok();
 
-    headers.conditional_extract_to_current_span(tracer.use_otel());
-    let mut fbb = FlatBufferBuilder::new();
+    let mut fbb: FlatBufferBuilder<'_> = FlatBufferBuilder::new();
     let num_total_pulses = message_processor.process(&mut fbb, &message);
     tracing::Span::current().record("num_total_pulses", num_total_pulses);
     tracing::Span::current().record(