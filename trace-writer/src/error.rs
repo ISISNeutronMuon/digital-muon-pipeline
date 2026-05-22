350ba3a4317f7f59ee74fdd27766cdf14406ad5c
Dan Nixon <dan@dan-nixon.com>
2026-05-22 11:29:31 +0100

    wip

---
+++ trace-writer/src/error.rs
@@ -0,0 +1,17 @@
+//! Error types for the trace-writer.
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub(crate) enum TraceWriterError {
+    #[error("HDF5 error: {0}")]
+    Hdf5(#[from] hdf5::Error),
+
+    #[error("Missing flatbuffer field: {0}")]
+    MissingField(&'static str),
+
+    #[error("GPS timestamp conversion failed")]
+    TimestampConversionFailed,
+
+    #[error("String could not be written as HDF5 unicode: {0}")]
+    UnicodeConversionFailed(String),
+}
